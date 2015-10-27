547cc878482953c4504a7af061a1a6f7b0b71b29
Jair Reina <Jair.Reina@digitaslbi.com>
2015-10-26 21:24:44 -0500

    version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# gulp-fileinfo
+#gulp-fileinfo
+
+## Information
+
+<table>
+<tr>
+<td>Package</td><td>gulp-fileinfo</td>
+</tr>
+<tr>
+<td>Description</td>
+<td>Reads the names of the vynil files that are coming through the pipe or any other properties that you want. You can either show the file list in the console log or get an array with the list of files.</td>
+</tr>
+<tr>
+<td>Node Version</td>
+<td>>= 0.10</td>
+</tr>
+</table>
+
+## Usage
+
+```js
+var fileinfo = require('gulp-fileinfo');
+
+gulp.task('default', function() {
+  return gulp
+  				.src('./lib/*.js')
+  				.pipe(fileinfo());
+});
+```
+
+This will output in the console all the list of files coming through the pipe. 
+
+If you need to get the list of files in an array, you can pass a callback as part of the options, that function will receive the list of files in an array as a paramenter
+
+```js
+var fileinfo = require('gulp-fileinfo');
+
+gulp.task('default', function() {
+  var doSomethingWithTheFilesList = function (filesList){
+    //do something with the filesList array here
+  }
+  
+  gulp
+  	.src('./lib/*.js')
+		.pipe(fileinfo({ callback: doSomethingWithTheFilesList }));
+});
+
+```
+
+The fileList variable will be an array of strings containing the name of the files in the pipe.
+
+## Options
+
+By default, the plugin just reads the `relative` property of the files. If you want something different, you can:
+
+### properties
+You can get the value of one specific property different to 'relative' passing the name of the property you want to read, like this:
+```json
+{"properties": "path"}
+```
+
+You can also get In this case you need to set the properties prop as an array.
+```json
+{"properties": ['relative','path','basename']}
+```
+
+When the 'properties' value is an array, the filesList array will contain objects (instead of strings) with the properties desired for each file. For instance:
+
+```json
+[{"relative":"index.js", "path":"path/to/index.js", "basename":"" }]
+```
+
+You can get a list of the Vinyl files properties here: https://github.com/gulpjs/vinyl
+
+
---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var through = require('through2');
+
+//options is an object that can have two props
+//properties: could be a string with the name of one property that you want to ready of the file, or it can be an array of all the properties you want to get from the file
+//callback: could be any custom function that receives the files list as a parameter. If no callback is provided, the plugin simply calls console.log
+module.exports = function(options) {
+  options = options || {};
+  var getFileInfo = null,
+      propertyToGet,
+      defaults = {
+                    properties: 'relative',
+                    callback: function finalCallback (files){
+                      for(var i=0; i<files.length; i++){
+                        console.log(files[i]);
+                      }
+                    }
+                  };
+
+  options = Object.assign(defaults, options);
+
+  //the getProperty just reads one property from the file, the one in the properties var
+  var getProperty = function getProperty(file){
+    return file[options.properties];
+  };
+
+  //the getMultipleProperties gets all the properties from the file specified in the array options.properties
+  var getMultipleProperties = function getMultipleProperties(file){
+    var obj = {};
+    for(var i=0; i<options.properties.length; i++){
+      obj[options.properties[i]] = file[options.properties[i]];
+    }
+    return obj;
+  };
+
+  var filesList = new Array();
+
+
+  if( typeof options.properties == "string"){ //if properties is a string, then we're looking for one property only
+  
+    getFileInfo = getProperty;
+
+  }else if( options.properties instanceof Array){ //if it's an array, we'll return all the properties in an object
+
+    getFileInfo = getMultipleProperties;
+
+  }else{
+
+    throw new Error('The provided options to gulp-fileinfo are invalid');
+
+  }
+
+  function bufferContents(file, enc, cb) {
+    
+    filesList.push(getFileInfo(file));
+    
+    cb();
+  }
+
+  function endStream(cb) {
+    
+    options.callback(filesList);
+
+    cb();
+
+  }
+
+  return through.obj(bufferContents, endStream);
+};
